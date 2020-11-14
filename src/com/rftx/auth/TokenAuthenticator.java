b61504b7b3c3db595ff74aa674d02a73aa554ca5
Rock Chin <1010553892@qq.com>
2020-11-14 12:52:23 +0800

    add token list edit method

--- src/com/rftx/auth/TokenAuthenticator.java
+++ src/com/rftx/auth/TokenAuthenticator.java
@@ -10,6 +10,35 @@ public class TokenAuthenticator implements IAuthenticator {
     public String clientToken="";
     //tokens list,as server
     HashMap<String,String> authTokens=new HashMap<>();
+
+    /**
+     * set conn token 
+     * conn token will be send when RFTXClient conn a RFTXServer
+     * @param connToken
+     */
+    public void setConnToken(String connToken){
+        this.clientToken=connToken;
+    }
+    /**
+     * get conn token
+     * @return
+     */
+    public String getConnToken(){
+        return this.clientToken;
+    }
+    /**
+     * add a valid token
+     */
+    public void addValidToken(String name,String token){
+        this.authTokens.put(name, token);
+    }
+    /**
+     * remove a valid token by name
+     * @param name
+     */
+    public void removeValidToken(String name){
+        this.authTokens.remove(name);
+    }
     public HashMap<String,String> getAuthTokenMap(){
         return authTokens;
     }
